8995807ef0d9028acd9464a43230aa0d7824c5b0
Casper Post <cpost@f1r5s7.codam.nl>
2022-08-03 17:24:08 +0200

    Memory leak fix when stack is 1 long

--- shared/free_stacks.c
+++ shared/free_stacks.c
@@ -6,40 +6,13 @@
 /*   By: cpost <cpost@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/22 08:07:15 by cpost         #+#    #+#                 */
-/*   Updated: 2022/07/08 11:37:04 by cpost         ########   odam.nl         */
+/*   Updated: 2022/08/03 17:15:25 by cpost         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdlib.h>
-
-/**
-Child function of free_stacks.
- * @param[in] b The address of stack b
- * @return none
- */
-static void	free_stacks_child(t_stack **b)
-{
-	t_stack	*temp;
-	t_stack	*temp2;
-
-	if (*b)
-	{
-		temp = *b;
-		temp2 = temp;
-		while (temp->next)
-		{
-			temp = temp->next;
-			free(temp2);
-			temp2 = temp;
-			if (!temp->next)
-			{
-				free(temp);
-				break ;
-			}
-		}
-	}
-}
+#include <ft_printf.h>
 
 /**
 Free stack a and stack b
@@ -55,18 +28,21 @@ void	free_stacks(t_stack **a, t_stack **b)
 	if (*a)
 	{
 		temp = *a;
-		temp2 = temp;
-		while (temp->next)
+		while (temp)
 		{
+			temp2 = temp;
 			temp = temp->next;
 			free(temp2);
-			temp2 = temp;
-			if (!temp->next)
-			{
-				free(temp);
-				break ;
-			}
 		}
 	}
-	free_stacks_child(b);
+	if (*b)
+	{
+		temp = *b;
+		while (temp)
+		{
+			temp2 = temp;
+			temp = temp->next;
+			free(temp2);
+		}
+	}
 }
